641bf8a12e39363488c05675b42e9e4d7af0af9e
Gibson431 <timmaxgibson@gmail.com>
2025-03-26 18:30:10 +1100

    add quanta_entangler_rp2040 board

--- app/boards/quanta/entangler_rp2040/board.cmake
+++ app/boards/quanta/entangler_rp2040/board.cmake
@@ -1,18 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# This configuration allows selecting what debug adapter debugging rpi_pico
-# by a command-line argument.
-# It is mainly intended to support both the 'picoprobe' and 'raspberrypi-swd'
-# adapter described in "Getting started with Raspberry Pi Pico".
-# And any other SWD debug adapter might also be usable with this configuration.
-
-# Set RPI_PICO_DEBUG_ADAPTER to select debug adapter by command-line arguments.
-# e.g.) west build -b rpi_pico -- -DRPI_PICO_DEBUG_ADAPTER=raspberrypi-swd
-# The value is treated as a part of an interface file name that
-# the debugger's configuration file.
-# The value must be the 'stem' part of the name of one of the files
-# in the openocd interface configuration file.
-# The setting is store to CMakeCache.txt.
 if("${RPI_PICO_DEBUG_ADAPTER}" STREQUAL "")
 	set(RPI_PICO_DEBUG_ADAPTER "cmsis-dap")
 endif()
